c6cc107f3157ed486e40ea737a3a89ce25cc6747
viho-kernel <vihari.reddy1802@gmail.com>
2026-01-30 17:01:09 +0530

    latest commit

--- create_instance.sh
+++ create_instance.sh
@@ -17,7 +17,7 @@ do
     --image-id $AMI_ID \
     --instance-type t3.medium \
     --security-group-ids $SG_ID \
-    --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$instance}]' \
+    --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=${instance}}]' \
     --query 'Instances[0].InstanceId' \
     --output text
 
@@ -29,12 +29,12 @@ do
     --image-id $AMI_ID \
     --instance-type t3.micro \
     --security-group-ids $SG_ID \
-    --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$instance}]' \
+    --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=${instance}}]' \
     --query 'Instances[0].InstanceId' \
     --output text
 
     fi
 
    )
-    echo " Instance ID of ${instance} is $INSTANCE_ID "
+    echo " Instance ID of ${instance} is '$INSTANCE_ID' "
 done
