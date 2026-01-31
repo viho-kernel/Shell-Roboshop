9e0e951adbe199ef19cdfa9ea05c5fc432098745
viho-kernel <vihari.reddy1802@gmail.com>
2026-01-31 16:53:35 +0530

    latest commit

--- frontend.sh
+++ frontend.sh
@@ -30,36 +30,36 @@ VALIDATE() {
     fi
 }
 
-dnf module disable nginx -y &>>$LOG_FILE
+dnf module disable nginx -y &>> $LOG_FILE
 VALIDATE $? "Disabling default nginx"
 
-dnf module enable nginx:1.24 -y &>>$LOG_FILE
+dnf module enable nginx:1.24 -y &>> $LOG_FILE
 VALIDATE $? "Enabling nginx 1.24 Version"
 
-dnf install nginx -y &>>$LOG_FILE
+dnf install nginx -y &>> $LOG_FILE
 VALIDATE $? "Installing Nginx -y"
 
-systemctl enable nginx &>>$LOG_FILE
+systemctl enable nginx &>> $LOG_FILE
 
 VALIDATE $? "Enabling Nginx"
 
-systemctl start nginx &>>$LOG_FILE
+systemctl start nginx &>> $LOG_FILE
 
 VALIDATE $? "Starting Nginx"
 
-rm -rf /usr/share/nginx/html/* &>>$LOG_FILE
+rm -rf /usr/share/nginx/html/* &>> $LOG_FILE
 VALIDATE $? "Removing default nginx"
 
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>$LOG_FILE
 VALIDATE $? "Downloading Frontend Content"
 
-cd /usr/share/nginx/html $>>$LOG_FILE
+cd /usr/share/nginx/html $>> $LOG_FILE
 
-unzip /tmp/frontend.zip &>>$LOG_FILE
+unzip /tmp/frontend.zip &>> $LOG_FILE
 VALIDATE $? "Unzipping frontend content"
 
-cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf &>>$LOG_FILE
+cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf &>> $LOG_FILE
 VALIDATE $? "Creating Reverse Proxy configuration"
 
-systemctl restart nginx &>>$LOG_FILE
+systemctl restart nginx &>> $LOG_FILE
 VALIDATE $? "Restarting Nginx Service"
